4c4ab1a7ac27969ba056a41d788d7a527f98f2f5
Archangel <radishikerov@yahoo.com>
2017-06-06 15:16:33 +0300

    Add Xamarin Froms Application

---
+++ XamarinForms/iOS/PhoneDialer.cs
@@ -0,0 +1,17 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using XamarinForms.iOS;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace XamarinForms.iOS
+{
+	public class PhoneDialer : IDialer
+	{
+		public bool Dial(string number)
+		{
+			return UIApplication.SharedApplication.OpenUrl(
+				new NSUrl("tel:" + number));
+		}
+	}
+}
