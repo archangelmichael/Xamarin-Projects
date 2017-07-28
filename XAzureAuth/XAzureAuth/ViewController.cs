516516f426e98cf5effb98ed0964e9f3bb50d2e9
Archangel <radishikerov@yahoo.com>
2017-07-28 15:24:27 +0300

    Create azure ad auth test app

--- XAzureAuth/XAzureAuth/ViewController.cs
+++ XAzureAuth/XAzureAuth/ViewController.cs
@@ -1,11 +1,23 @@
 ﻿using System;
-
+using System.Linq;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using UIKit;
 
 namespace XAzureAuth
 {
 	public partial class ViewController : UIViewController
 	{
+		//Client ID from from step 1. point 6
+		public static string clientId = "25927d3c-.....-63f2304b90de";
+		public static string commonAuthority = "https://login.windows.net/common";
+//Redirect URI from step 1. point 5<br />
+		public static Uri returnUri = new Uri("http://xam-demo-redirect");
+		//Graph URI if you've given permission to Azure Active Directory in step 1. point 6
+		const string graphResourceUri = "https://graph.windows.net";
+		public static string graphApiVersion = "2013-11-08";
+		//AuthenticationResult will hold the result after authentication completes
+		AuthenticationResult authResult = null;
+
 		protected ViewController(IntPtr handle) : base(handle)
 		{
 			// Note: this .ctor should not contain any initialization logic.
@@ -14,7 +26,18 @@ namespace XAzureAuth
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
-			// Perform any additional setup after loading the view, typically from a nib.
+
+
+
+		}
+
+		async void OpenADAuth()
+		{
+var authContext = new AuthenticationContext(commonAuthority);
+  if (authContext.TokenCache.ReadItems().Count() > 0)
+    authContext = new AuthenticationContext(authContext.TokenCache.ReadItems().First().Authority);
+ 
+			authResult = await authContext.AcquireTokenAsync(graphResourceUri, clientId, returnUri, new PlatformParameters(this));
 		}
 
 		public override void DidReceiveMemoryWarning()
