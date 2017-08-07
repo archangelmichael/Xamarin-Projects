3335bbe53dc2e95dc2cec812fe05a2018cce0b8f
Archangel <radishikerov@yahoo.com>
2017-08-07 21:05:52 +0300

    Delete Bad Project. Create new Calendar project. Add custom Calendar view to project.

--- XMyCalendar/XMyCalendar/Main.cs
+++ XMyCalendar/XMyCalendar/Main.cs
@@ -1,6 +1,6 @@
 ﻿using UIKit;
 
-namespace XCalendar
+namespace XMyCalendar
 {
 	public class Application
 	{
---
+++ XMyCalendar/XMyCalendar/ViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UIKit;
+
+namespace XMyCalendar
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle) { }
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+		}
+
+		public override void ViewDidAppear(bool animated)
+		{
+			base.ViewDidAppear(animated);
+			ShowCalendar();
+		}
+
+		void ShowCalendar()
+		{
+			
+		}
+	}
+}
