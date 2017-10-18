74144dacc6a7e4c8c01ef26440c208cf353273a4
Radi <radishikerov@yahoo.com>
2017-10-18 14:44:16 +0300

    Use ADAL library to authenticate with azure

---
+++ XAzureAuth/XAzureAuth/Authentication/OAuth/OAuthCodeHttpHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XAzureAuth.Authentication
+{
+    public class OAuthCodeHttpHandler : HttpClientHandler
+    {
+        private readonly string username;
+        private readonly string samltoken;
+
+        public OAuthCodeHttpHandler(string username, string samltoken)
+		{
+			this.username = username;
+			this.samltoken = samltoken;
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			string credentials = username + ":" + samltoken;
+
+			request.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(credentials)));
+
+			return base.SendAsync(request, cancellationToken);
+		}
+    }
+}
---
+++ XAzureAuth/XAzureAuth/Authentication/OAuth/OAuthRequestHttpHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XAzureAuth.Authentication
+{
+	public class OAuthRequestHttpHandler : HttpClientHandler
+	{
+		private string OAuthToken;
+
+		public OAuthRequestHttpHandler(string OAuthToken)
+		{
+			this.OAuthToken = OAuthToken;
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			request.Headers.Add("Authorization", "Bearer " + OAuthToken);
+
+			return base.SendAsync(request, cancellationToken);
+		}
+	}
+}
